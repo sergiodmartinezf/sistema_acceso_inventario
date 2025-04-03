a3c6b505957d8a23ea7b0c6c6523e28ec70faa11
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2025-04-03 20:48:47 -0300

    creación app

--- sistema_acceso_inventario/sistema_acceso_inventario/settings.py
+++ sistema_acceso_inventario/sistema_acceso_inventario/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'sistema_acceso_inventario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sistema_acceso_inventario',
+        'USER': 'postgres',
+        'PASSWORD': 'compuvi',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
